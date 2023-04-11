a18f7d2d7f5616282b243be4dce79cdcfefbbb66
EddieEdisonTUT <serj9707@gmail.com>
2023-04-11 21:38:00 +0300

    Dorobyv DZ_1

---
+++ DZ/Chastyna2/Zav2_Ch2.py
@@ -0,0 +1,4 @@
+number1 = int(input('Зарплатня: '))
+number2 = int(input('Кредит: '))
+number3 = int(input('Комунальні: '))
+print(number1-number2-number3)
---
+++ DZ/Chastyna3/Zav3_Ch3.py
@@ -0,0 +1,6 @@
+# Введення кількість метрів
+number1 = int(input('Метрів: '))
+print(number1*100,'cm')
+print(number1*10,'dm')
+print(number1*1000,'mm')
+print(number1*0.00062,'mil')
