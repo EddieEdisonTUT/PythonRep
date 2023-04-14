fe0b720d12a250760d66c2a8e1965b672d656fb2
EddieEdisonTUT <serj9707@gmail.com>
2023-04-14 18:59:47 +0300

    Zrobyv DZ_2

--- DZ_2/Chastyna_1/Zav_2_Ch1.py
+++ DZ_2/Chastyna_1/Zav_2_Ch1.py
@@ -1,16 +1,15 @@
 number1 = int(input('First number: '))
 number2 = int(input('Second number: '))
 number3 = int(input('Thrid number: '))
+
 print('Зробіть вибір')
 print('1: Максимум')
 print('2: Мінімум')
 print('3: Середне')
 choise = int(input('Ваш вибір: '))
 if choise == 1:
-    elif(number1 > nu)
-    print(number1 + number2 + number3)
+    print()
 elif choise == 2:
-    print(number1 * number2 * number3)
+    print()
 elif choise == 3:
-    print((number1 + number2 + number3) // 2)
-    
+    print((number1 + number2 + number3) // 3)
