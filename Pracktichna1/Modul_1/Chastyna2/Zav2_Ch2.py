c7d5c111e06c6f0b2ced6f3e5c21a07830a315c2
EddieEdisonTUT <serj9707@gmail.com>
2023-04-17 21:28:31 +0300

    kapec

--- Pracktichna1/Modul_1/Chastyna2/Zav2_Ch2.py
+++
@@ -1,5 +0,0 @@
-print('\"Anyone who')
-print('    stops')
-print('       learning is old\,')
-print('          whether at twenty or eighty\".')
-print('                                  Henry Ford')
--- Pracktichna2/Chastyna2/task_2.py
+++
@@ -1,5 +0,0 @@
-number1 = int(input('Введіть діаметр: '))
-print('Зробіть вибір')
-print('1: Площща')
-print('2: Периметр')
-choice = int(input('Ваш вибір: '))
