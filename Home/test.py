1ad949a56bdc9d6b13daff44c813cdb094550efb
EddieEdisonTUT <serj9707@gmail.com>
2023-04-22 22:18:29 +0300

    V DZ_3 zrobyv Zav_3 and Zav_4

--- Home/test.py
+++ Home/test.py
@@ -4,46 +4,46 @@ manager3 = int(input('Менеджер 3: '))
 stavka = 200
 if manager1 > manager2 and manager1 > manager3 and manager1 < 500:
     print('Кращій М1:',(manager1 * 0.03) + stavka + 200)
-    if manager2 < 500 and manager3 < 500:
-        print('M2:',(manager2 * 0.03) + stavka)
-        print('M3:',(manager3 * 0.03) + stavka)
+elif manager2 < 500 and manager3 < 500:
+    print('M2:',(manager2 * 0.03) + stavka)
+    print('M3:',(manager3 * 0.03) + stavka)
 elif manager2 > manager1 and manager2 > manager3 and manager2 < 500:
     print('Кращій М2:',(manager2 * 0.03) + stavka + 200)
-    if manager1 < 500 and manager3 < 500:
-        print('M1:',(manager1 * 0.03) + stavka)
-        print('M3:',(manager3 * 0.03) + stavka)
+elif manager1 < 500 and manager3 < 500:
+    print('M1:',(manager1 * 0.03) + stavka)
+    print('M3:',(manager3 * 0.03) + stavka)
 elif manager3 > manager1 and manager3 > manager2 and manager3 < 500:
     print('Кращій М3:',(manager3 * 0.03) + stavka + 200)
-    if manager1 < 500 and manager2 < 500:
-        print('M1:',(manager1 * 0.03) + stavka)
-        print('M2:',(manager2 * 0.03) + stavka)
+elif manager1 < 500 and manager2 < 500:
+    print('M1:',(manager1 * 0.03) + stavka)
+    print('M2:',(manager2 * 0.03) + stavka)
 if manager1 > manager2 and manager1 > manager3 and manager1 >= 500 and manager1 < 1000:
     print('Кращій М1:',(manager1 * 0.05) + stavka + 200)
-    if manager2 >= 500 and manager2 < 1000 and manager3 >= 500 and manager3 < 1000:
-        print('M2:',(manager2 * 0.05) + stavka)
-        print('M3:',(manager3 * 0.05) + stavka)
+elif manager2 >= 500 and manager2 < 1000 and manager3 >= 500 and manager3 < 1000:
+    print('M2:',(manager2 * 0.05) + stavka)
+    print('M3:',(manager3 * 0.05) + stavka)
 elif manager2 > manager1 and manager2 > manager3 and manager2 >= 500 and manager2 < 1000:
     print('Кращій М2:',(manager2 * 0.02) + stavka + 200)
-    if manager1 >= 500 and manager1 < 1000 and manager3 >= 500 and manager3 < 1000:
-        print('M1:',(manager1 * 0.05) + stavka)
-        print('M3:',(manager3 * 0.05) + stavka)
+elif manager1 >= 500 and manager1 < 1000 and manager3 >= 500 and manager3 < 1000:
+    print('M1:',(manager1 * 0.05) + stavka)
+    print('M3:',(manager3 * 0.05) + stavka)
 elif manager3 > manager1 and manager3 > manager2 and manager3 >= 500 and manager3 < 1000:
     print('Кращій М3:',(manager3 * 0.02) + stavka + 200)
-    if manager1 >= 500 and manager1 < 1000 and manager2 >= 500 and manager2 < 1000:
-        print('M1:',(manager1 * 0.05) + stavka)
-        print('M2:',(manager2 * 0.05) + stavka)
+elif manager1 >= 500 and manager1 < 1000 and manager2 >= 500 and manager2 < 1000:
+    print('M1:',(manager1 * 0.05) + stavka)
+    print('M2:',(manager2 * 0.05) + stavka)
 if manager1 > manager2 and manager1 > manager3 and manager1 > 1000:
     print('Кращій М1:',(manager1 * 0.08) + stavka + 200)
-    if manager2 > 1000 and manager3 > 1000:
-        print('M2:',(manager2 * 0.08) + stavka)
-        print('M3:',(manager3 * 0.08) + stavka)
+elif manager2 > 1000 and manager3 > 1000:
+    print('M2:',(manager2 * 0.08) + stavka)
+    print('M3:',(manager3 * 0.08) + stavka)
 elif manager2 > manager1 and manager2 > manager3 and manager2 > 1000:
     print('Кращій М2:',(manager2 * 0.08) + stavka + 200)
-    if manager1 > 1000 and manager3 > 1000:
-        print('M1:',(manager1 * 0.08) + stavka)
-        print('M3:',(manager3 * 0.08) + stavka)
+elif manager1 > 1000 and manager3 > 1000:
+    print('M1:',(manager1 * 0.08) + stavka)
+    print('M3:',(manager3 * 0.08) + stavka)
 elif manager3 > manager1 and manager3 > manager2 and manager3 > 1000:
     print('Кращій М3:',(manager3 * 0.08) + stavka + 200)
-    if manager1 > 1000 and manager2 > 1000:
-        print('M1:',(manager1 * 0.08) + stavka)
-        print('M2:',(manager2 * 0.08) + stavka)
+elif manager1 > 1000 and manager2 > 1000:
+    print('M1:',(manager1 * 0.08) + stavka)
+    print('M2:',(manager2 * 0.08) + stavka)
