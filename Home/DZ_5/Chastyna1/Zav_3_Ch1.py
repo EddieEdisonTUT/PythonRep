529b8a8e31fb8211a1d0076d9f57fdfb09530171
EddieEdisonTUT <serj9707@gmail.com>
2023-05-17 17:00:50 +0300

    Zav_3_Ch1 string

--- Home/DZ_5/Chastyna1/Zav_3_Ch1.py
+++ Home/DZ_5/Chastyna1/Zav_3_Ch1.py
@@ -2,7 +2,7 @@ text = input("Введіть текст: ")
 
 delimiters = [".", "!", "?"]
 
-rechennya = [for rechennya in text ]
+rechennya = [rechennya.strip() for rechennya in text.split((delimiters), text) ]
 
 num_sentences = len(rechennya)
 print("Кількість речень у тексті: ", num_sentences)
