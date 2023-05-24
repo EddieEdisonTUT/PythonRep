65cfe4e8a10898f1b06ab00163bab44a417fca23
EddieEdisonTUT <serj9707@gmail.com>
2023-05-24 17:13:42 +0300

    Naviv poryadok v DZ_5

--- Home/DZ_6/Chastyna1/Zav_1 _Ch1.py
+++
@@ -1,5 +0,0 @@
-def display_txt():
-    print ('"Don`t compare yourself with anyone in this world... \nif you do so, you are insulting yourself."')
-    print('                                             Bill Gates')
-
-display_txt()
