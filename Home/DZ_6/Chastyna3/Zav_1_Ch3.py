327a2b7e05b119d5cfb226b035d304b2c5ad6629
student <student@kr.step>
2023-05-31 17:55:31 +0300

    Koreguvanya Zav_1Ch3

--- Home/DZ_6/Chastyna3/Zav_1_Ch3.py
+++ Home/DZ_6/Chastyna3/Zav_1_Ch3.py
@@ -1,10 +1,10 @@
-def gcd_recursive(a, b):
+def spylniy_dilnyk(a, b):
     if b == 0:
         return a
     else:
-        return gcd_recursive(b, a % b)
+        return spylniy_dilnyk(b, a % b)
 
-num1 = 36
-num2 = 24
-gcd = gcd_recursive(num1, num2)
+num1 = 15
+num2 = 45
+gcd = spylniy_dilnyk(num1, num2)
 print("Найбільший загальний дільник чисел", num1, "і", num2, "=", gcd)
